f04fb8e0c2348a5a93af7c5d4db4a601043cd3a7
Mikhail Ustinskov <ustinskov@yandex.ru>
2023-07-29 05:05:14 +0300

    Remove dead code

--- main/wifi_connect.c
+++ main/wifi_connect.c
@@ -242,36 +242,14 @@ esp_err_t example_wifi_connect(void)
     example_wifi_start();
     wifi_config_t wifi_config = {
         .sta = {
-#if !CONFIG_EXAMPLE_WIFI_SSID_PWD_FROM_STDIN
             .ssid = CONFIG_EXAMPLE_WIFI_SSID,
             .password = CONFIG_EXAMPLE_WIFI_PASSWORD,
-#endif
             .scan_method = EXAMPLE_WIFI_SCAN_METHOD,
             .sort_method = EXAMPLE_WIFI_CONNECT_AP_SORT_METHOD,
             .threshold.rssi = CONFIG_EXAMPLE_WIFI_SCAN_RSSI_THRESHOLD,
             .threshold.authmode = EXAMPLE_WIFI_SCAN_AUTH_MODE_THRESHOLD,
         },
     };
-#if CONFIG_EXAMPLE_WIFI_SSID_PWD_FROM_STDIN
-    example_configure_stdin_stdout();
-    char buf[sizeof(wifi_config.sta.ssid)+sizeof(wifi_config.sta.password)+2] = {0};
-    ESP_LOGI(TAG, "Please input ssid password:");
-    fgets(buf, sizeof(buf), stdin);
-    int len = strlen(buf);
-    buf[len-1] = '\0'; /* removes '\n' */
-    memset(wifi_config.sta.ssid, 0, sizeof(wifi_config.sta.ssid));
-
-    char *rest = NULL;
-    char *temp = strtok_r(buf, " ", &rest);
-    strncpy((char*)wifi_config.sta.ssid, temp, sizeof(wifi_config.sta.ssid));
-    memset(wifi_config.sta.password, 0, sizeof(wifi_config.sta.password));
-    temp = strtok_r(NULL, " ", &rest);
-    if (temp) {
-        strncpy((char*)wifi_config.sta.password, temp, sizeof(wifi_config.sta.password));
-    } else {
-        wifi_config.sta.threshold.authmode = WIFI_AUTH_OPEN;
-    }
-#endif
     return example_wifi_sta_do_connect(wifi_config, true);
 }
 

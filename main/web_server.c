962e2fc85cc3e9e8f417d1ae1df17644d8f884a2
Mikhail Ustinskov <ustinskov@yandex.ru>
2025-10-13 06:45:36 +0300

    Add SPIFFS update support

--- main/web_server.c
+++ main/web_server.c
@@ -53,7 +53,8 @@ static bool mcu_restart_request = false;
 
 static esp_err_t system_info_get_handler(httpd_req_t *req);
 static esp_err_t mcu_restart_handler(httpd_req_t *req);
-static esp_err_t ota_post_handler(httpd_req_t *req);
+static esp_err_t firmware_update_post_handler(httpd_req_t *req);
+static esp_err_t spiffs_update_post_handler(httpd_req_t *req);
 static esp_err_t rest_common_get_handler(httpd_req_t *req);
 static esp_err_t console_ws_receive_handler(httpd_req_t *req, httpd_ws_frame_t *pkt);
 
@@ -141,10 +142,23 @@ esp_err_t web_start(const char *base_path)
     httpd_uri_t firmware_update_uri = {
         .uri = "/api/firmware_update",
         .method = HTTP_POST,
-        .handler = ota_post_handler,
+        .handler = firmware_update_post_handler,
         .user_ctx = &context
     };
     ret = httpd_register_uri_handler(server, &firmware_update_uri);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Cannot register URI handler");
+        return ret;
+    }
+
+    /* URI handler for /spiffs_update */
+    httpd_uri_t spiffs_update_uri = {
+        .uri = "/api/spiffs_update",
+        .method = HTTP_POST,
+        .handler = spiffs_update_post_handler,
+        .user_ctx = &context
+    };
+    ret = httpd_register_uri_handler(server, &spiffs_update_uri);
     if (ret != ESP_OK) {
         ESP_LOGE(TAG, "Cannot register URI handler");
         return ret;
@@ -326,7 +340,7 @@ static esp_err_t mcu_restart_handler(httpd_req_t *req) {
     return ESP_OK;
 }
 
-static esp_err_t ota_post_handler(httpd_req_t *req)
+static esp_err_t firmware_update_post_handler(httpd_req_t *req)
 {
     server_context_t *rest_context = (server_context_t *)req->user_ctx;
     esp_err_t ret = ESP_OK;
@@ -341,8 +355,8 @@ static esp_err_t ota_post_handler(httpd_req_t *req)
         return ESP_FAIL;
     }
 
-    ESP_LOGI(TAG, "Writing partition: type %d, subtype %d, offset 0x%08x", update_prt->type, update_prt->subtype, update_prt->address);
-    ESP_LOGI(TAG, "Running partition: type %d, subtype %d, offset 0x%08x", running_prt->type, running_prt->subtype, running_prt->address);
+    ESP_LOGI(TAG, "Writing partition: type %d/%d off 0x%08x size %d", update_prt->type, update_prt->subtype, update_prt->address, update_prt->size);
+    ESP_LOGI(TAG, "Running partition: type %d/%d off 0x%08x size %d", running_prt->type, running_prt->subtype, running_prt->address, update_prt->size);
 
     ret = esp_ota_begin(update_prt, OTA_WITH_SEQUENTIAL_WRITES, &update_handle);
     if (ret != ESP_OK) {
@@ -359,7 +373,7 @@ static esp_err_t ota_post_handler(httpd_req_t *req)
         }
         size_t bytes_read = ret;
         remaining -= bytes_read;
-        ESP_LOGI(TAG, "Received %d bytes, remaining %d", bytes_read, remaining);
+        ESP_LOGD(TAG, "Received %d bytes, remaining %d", bytes_read, remaining);
         ret = esp_ota_write(update_handle, rest_context->scratch, bytes_read);
         if (ret != ESP_OK) {
             ESP_LOGE(TAG, "esp_ota_write failed with %d (%s)", ret, esp_err_to_name(ret));
@@ -392,6 +406,65 @@ static esp_err_t ota_post_handler(httpd_req_t *req)
     httpd_resp_set_type(req, "text/plain");
     httpd_resp_set_hdr(req, "Connection", "close");
     httpd_resp_sendstr(req, "Updated successfully, MCU restart pending");
+    httpd_resp_send(req, NULL, 0);
+    return ESP_OK;
+}
+
+static esp_err_t spiffs_update_post_handler(httpd_req_t *req)
+{
+    server_context_t *rest_context = (server_context_t *)req->user_ctx;
+    esp_err_t ret = ESP_OK;
+    int remaining = req->content_len;
+    const esp_partition_t *spiffs_prt = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_SPIFFS, NULL);
+    uint32_t wrote_size = 0;
+
+    if (spiffs_prt == NULL) {
+        ESP_LOGE(TAG, "esp_partition_find_first failed");
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(TAG, "SPIFFS partition: type %d/%d off 0x%08x size %d", spiffs_prt->type, spiffs_prt->subtype, spiffs_prt->address, spiffs_prt->size);
+
+    if (remaining != spiffs_prt->size) {
+        ESP_LOGE(TAG, "Content length does not match partition size");
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+
+    ret = esp_partition_erase_range(spiffs_prt, 0, spiffs_prt->size);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "esp_partition_erase_range failed with %d (%s)", ret, esp_err_to_name(ret));
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+
+    while (remaining > 0) {
+        // Read the data for the request
+        if ((ret = httpd_req_recv(req, rest_context->scratch, MIN(remaining, SCRATCH_BUFSIZE))) <= 0) {
+            ret = ESP_FAIL;
+            break;
+        }
+        size_t bytes_read = ret;
+        remaining -= bytes_read;
+        ESP_LOGD(TAG, "Received %d bytes, remaining %d", bytes_read, remaining);
+        ret = esp_partition_write(spiffs_prt, wrote_size, rest_context->scratch, bytes_read);
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG, "esp_partition_write failed with %d (%s)", ret, esp_err_to_name(ret));
+            break;
+        }
+        wrote_size += bytes_read;
+    }
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Upload failed");
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(TAG, "Updated successfully");
+
+    httpd_resp_set_type(req, "text/plain");
+    httpd_resp_sendstr(req, "Updated successfully");
     httpd_resp_send(req, NULL, 0);
     return ESP_OK;
 }
